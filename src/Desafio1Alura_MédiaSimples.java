0de04458b6ab66c2f416090ddeab838ad27415ff
RFXXS-DEV <rfxxs.dev@gmail.com>
2026-05-29 16:29:16 -0300

    Mudanças no nome dos arquivos, e descrição adicionada dentro do código de cada desafio

---
+++ src/Desafio1Alura_MédiaSimples.java
@@ -0,0 +1,11 @@
+public class Desafio1Alura_MédiaSimples {
+    static void main() {
+        // No desafio Prático pedia para eu colocar 2 notas e calcular a média delas
+        int nota1 = 9;
+        int nota2 = 6;
+        int media = (nota1 + nota2) / 2;
+
+        System.out.println("Suas notas foram: " + nota1 + " e " + nota2 + ". Você ficou com: " + media);
+
+    }
+}
---
+++ src/Desafio2Alura_Casting.java
@@ -0,0 +1,8 @@
+public class Desafio2Alura_Casting {
+    static void main() {
+    /* Neste desafio prático é para declarar 2 variáveis de tipos diferentes,
+       fazer o casting delas e imprimir isso no terminal
+    */
+
+    }
+}
